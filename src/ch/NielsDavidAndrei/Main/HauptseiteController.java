0e1b36a438959c8432a1e56ac9c8f1e02c369749
Andrei Oleniuc <andreipsn14@gmail.com>
2020-08-19 14:59:49 +0200

    ini

--- src/ch/NielsDavidAndrei/Main/HauptseiteController.java
+++ src/ch/NielsDavidAndrei/Main/HauptseiteController.java
@@ -22,6 +22,7 @@ public class HauptseiteController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        System.out.println("Hqallo");
     }    
     
 }
