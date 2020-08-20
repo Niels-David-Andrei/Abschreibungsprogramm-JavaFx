ed9d6ad2cb446b25cfb65572dd192b3825ec99da
Andrei Oleniuc <andreipsn14@gmail.com>
2020-08-20 08:37:14 +0200

    Standard Struktur

--- src/ch/NielsDavidAndrei/Modeling/Object.java
+++ src/ch/NielsDavidAndrei/Modeling/Object.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ch.NieslDavidAndrei.Modeling;
+package ch.NielsDavidAndrei.Modeling;
 
 /**
  *
