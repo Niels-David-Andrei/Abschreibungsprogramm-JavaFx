bdda6b216d4ce87460fb8848c89fede0246cab7c
Andrei Oleniuc <andreipsn14@gmail.com>
2020-09-08 17:55:55 +0200

    Dragable Scene

--- src/ch/NielsDavidAndrei/Starter.java
+++ src/ch/NielsDavidAndrei/Starter.java
@@ -101,6 +101,21 @@ public class Starter extends Application {
         Parent root;
         root = loader.load();
 
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
         AusgabeAnsichtController controller = loader.getController();
         final Scene scene = new Scene(root);
         stage.setTitle("Eingabe");
